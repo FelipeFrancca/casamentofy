398e91ab43e6b94d74f3a696f36a3e58943570d3
FelipeFrancca <suporte.felipefranca@gmail.com>
2023-04-27 13:34:48 -0300

    up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+//MUI imports
+import { Box, Typography } from "@mui/material";
+//MUI imports end
+
+//Fonts imports
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+//Fonts imports end
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Box>
+          <Typography variant="h1" fontWeight={700}>
+            Meu Casamento <br/>
+            😭💸
+          </Typography>
+        </Box>
       </header>
     </div>
   );
