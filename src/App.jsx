324e4a26d7bfe9fb5ce32476b375a52f9364eb0f
FelipeFrancca <suporte.felipefranca@gmail.com>
2023-04-27 16:59:47 -0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import "./App.css";
 
 //MUI imports
-import { Box, Typography } from "@mui/material";
+import { Box, } from "@mui/material";
+import Card from "./Pages/Assets/Components/Card";
 //MUI imports end
+
+import imgliquidificador from './Pages/Assets/img/imgteste.jpg';
 
 //Fonts imports
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import Album from "./Pages/Album";
 //Fonts imports end
 
 function App() {
   return (
     <div className="App">
-      <Album></Album>
+      <Box>
+        <Card nome="Liquidificador" link="https://mui.com/material-ui/react-card/" img={imgliquidificador}></Card>
+      </Box>
     </div>
   );
 }
