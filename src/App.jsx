5d1b2ea4237d21db63093450f609f29320ac47b7
FelipeFrancca <suporte.felipefranca@gmail.com>
2023-04-28 16:56:52 -0300

    update tarde

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Card from "./Pages/Assets/Components/Card";
 //MUI imports end
 
 import imgliquidificador from "./Pages/Assets/img/default.png";
+import Carrossel from "./Pages/Assets/Components/Carrossel";
 
 //Fonts imports
 import "@fontsource/roboto/300.css";
@@ -16,40 +17,68 @@ import "@fontsource/roboto/700.css";
 
 function App() {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        flexWrap: "wrap",
-        maxWidth: "1200px",
-      }}
-      p={5}
-    >
-      <Box className="text-focus-in" sx={{ textAlign: "center" }}>
-        <Typography variant="h1" fontWeight={700}>
-          Bem vindo ao nosso casamento
-        </Typography>
-        <Typography variant="h5" fontWeight={700}>
-          Por: Felipe e Elina França
-        </Typography>
-      </Box>
-      <Box className="tracking-in-contract-bck-bottom"
+    <Box>
+      <Carrossel></Carrossel>
+      <Box
         sx={{
           display: "flex",
-          flexDirection: "row",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          flexWrap: "wrap",
+          maxWidth: "100%",
+          maxHeight: "100%",
         }}
-        p={20}
-        fontWeight={700}
+        p={5}
       >
-        <Card
-          nome="Liquidificador" link="https://m.casasbahia.com.br/liquidificador-mondial-turbo-power-l-99-fb-preto-500w-com-3-velocidades-50004213/p/50004214" img={imgliquidificador}></Card>
-        <Card nome="Liquidificador" link="https://bit.ly/lipeejoyce" img={imgliquidificador}></Card>
-        <Card nome="Liquidificador" link="https://bit.ly/lipeejoyce" img={imgliquidificador}></Card>
-        <Card nome="Liquidificador" link="https://bit.ly/lipeejoyce" img={imgliquidificador}></Card>
-        <Card nome="Liquidificador" link="https://bit.ly/lipeejoyce" img={imgliquidificador}></Card>
-        <Card nome="Liquidificador" link="https://bit.ly/lipeejoyce" img={imgliquidificador}></Card>
+        <Box className="text-focus-in" sx={{ textAlign: "center" }}>
+          <Typography variant="h1" fontWeight={700}>
+            Bem vindo ao nosso casamento
+          </Typography>
+          <Typography variant="h5" fontWeight={700}>
+            Por: Felipe e Elina França
+          </Typography>
+        </Box>
+        <Box
+          className="tracking-in-contract-bck-bottom"
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+          }}
+          p={20}
+          fontWeight={700}
+        >
+          <Card
+            nome="Liquidificador"
+            link="https://m.casasbahia.com.br/liquidificador-mondial-turbo-power-l-99-fb-preto-500w-com-3-velocidades-50004213/p/50004214"
+            img={imgliquidificador}
+          ></Card>
+          <Card
+            nome="Liquidificador"
+            link="https://bit.ly/lipeejoyce"
+            img={imgliquidificador}
+          ></Card>
+          <Card
+            nome="Liquidificador"
+            link="https://bit.ly/lipeejoyce"
+            img={imgliquidificador}
+          ></Card>
+          <Card
+            nome="Liquidificador"
+            link="https://bit.ly/lipeejoyce"
+            img={imgliquidificador}
+          ></Card>
+          <Card
+            nome="Liquidificador"
+            link="https://bit.ly/lipeejoyce"
+            img={imgliquidificador}
+          ></Card>
+          <Card
+            nome="Liquidificador"
+            link="https://bit.ly/lipeejoyce"
+            img={imgliquidificador}
+          ></Card>
+        </Box>
       </Box>
     </Box>
   );
---
+++ src/Pages/Assets/Components/Carrossel.jsx
@@ -0,0 +1,122 @@
+import * as React from "react";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MobileStepper from "@mui/material/MobileStepper";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import SwipeableViews from "react-swipeable-views";
+import { autoPlay } from "react-swipeable-views-utils";
+import carrossel1 from "../img/carrossel/carrossel1.jpg";
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const images = [
+  {
+    imgPath:
+      "https://setimoregistrocivil.com.br/site/images/2020/09/16/casamento2.png",
+  },
+  {
+    imgPath:
+      "https://lapisdenoiva.com/wp-content/uploads/2020/01/casamento-no-caribe-ao-por-do-sol.jpg",
+  },
+  {
+    imgPath:
+      "https://home.lejour.com.br/wp-content/uploads/2021/09/espaco-para-casamento-rancho-21.jpg",
+  },
+];
+
+function SwipeableTextMobileStepper() {
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = images.length;
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStepChange = (step) => {
+    setActiveStep(step);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Paper
+        square
+        elevation={0}
+        sx={{
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <Typography>{images[activeStep].label}</Typography>
+      </Paper>
+      <AutoPlaySwipeableViews
+        axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+        index={activeStep}
+        onChangeIndex={handleStepChange}
+        enableMouseEvents
+      >
+        {images.map((step, index) => (
+          <div key={step.label}>
+            {Math.abs(activeStep - index) <= 2 ? (
+              <Box
+                component="img"
+                sx={{
+                  minHeight: "100px",
+                  maxHeight: "300px",
+                  display: "flex",
+                  overflow: "hidden",
+                  width: "100%",
+                  objectFit: "cover",
+                }}
+                src={step.imgPath}
+                alt={step.label}
+              />
+            ) : null}
+          </div>
+        ))}
+      </AutoPlaySwipeableViews>
+      <MobileStepper
+      sx={{background: "linear-gradient(180deg, rgba(2,0,36,1) 0%, rgba(52,22,113,1) 0%, rgba(255,255,255,1) 100%);",}}
+        steps={maxSteps}
+        position="static"
+        activeStep={activeStep}
+        nextButton={
+          <Button
+            size="small"
+            onClick={handleNext}
+            disabled={activeStep === maxSteps - 1}
+            sx={{color:"#ffffff"}}
+            
+          >
+            Próximo
+            {theme.direction === "rtl" ? (
+              <KeyboardArrowLeft />
+            ) : (
+              <KeyboardArrowRight />
+            )}
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0} sx={{color:"#ffffff"}}>
+            {theme.direction === "rtl" ? (
+              <KeyboardArrowRight />
+            ) : (
+              <KeyboardArrowLeft />
+            )}
+            Voltar
+          </Button>
+        }
+      />
+    </Box>
+  );
+}
+
+export default SwipeableTextMobileStepper;
